18e23682ebf1742969435c3129eb6a714737f30f
pawn <zengyimou@sina.com>
2015-01-16 16:30:21 +0800

    修复无法判断正确分享类型的bug

--- proj.android/src/cn/sharesdk/ShareSDKUtils.java
+++ proj.android/src/cn/sharesdk/ShareSDKUtils.java
@@ -16,17 +16,19 @@ import android.content.Context;
 import android.os.Message;
 import android.os.Handler.Callback;
 import android.text.TextUtils;
+import android.util.Log;
+import android.widget.Toast;
 
 public class ShareSDKUtils {
 	private static boolean DEBUG = true;
 	private static Context context;
 	private static PlatformActionListener paListaner;
 	private static Hashon hashon;
-	
+
 	private ShareSDKUtils() {
-		
+
 	}
-	
+
 	public static void prepare() {
 		UIHandler.prepare();
 		context = Cocos2dxActivity.getContext().getApplicationContext();
@@ -38,18 +40,20 @@ public class ShareSDKUtils {
 						onJavaCallback((String) msg.obj);
 					}
 				});
-				
+
 				return false;
 			}
 		};
 		paListaner = new PlatformActionListener() {
-			public void onComplete(Platform platform, int action, HashMap<String, Object> res) {
+			public void onComplete(Platform platform, int action,
+					HashMap<String, Object> res) {
 				if (DEBUG) {
 					System.out.println("onComplete");
 					System.out.println(res == null ? "" : res.toString());
 				}
 				HashMap<String, Object> map = new HashMap<String, Object>();
-				map.put("platform", ShareSDK.platformNameToId(platform.getName()));
+				map.put("platform",
+						ShareSDK.platformNameToId(platform.getName()));
 				map.put("action", action);
 				map.put("status", 1); // Success = 1, Fail = 2, Cancel = 3
 				map.put("res", res);
@@ -57,14 +61,15 @@ public class ShareSDKUtils {
 				msg.obj = hashon.fromHashMap(map);
 				UIHandler.sendMessage(msg, cb);
 			}
-			
+
 			public void onError(Platform platform, int action, Throwable t) {
 				if (DEBUG) {
 					System.out.println("onError");
 					t.printStackTrace();
 				}
 				HashMap<String, Object> map = new HashMap<String, Object>();
-				map.put("platform", ShareSDK.platformNameToId(platform.getName()));
+				map.put("platform",
+						ShareSDK.platformNameToId(platform.getName()));
 				map.put("action", action);
 				map.put("status", 2); // Success = 1, Fail = 2, Cancel = 3
 				map.put("res", throwableToMap(t));
@@ -72,13 +77,14 @@ public class ShareSDKUtils {
 				msg.obj = hashon.fromHashMap(map);
 				UIHandler.sendMessage(msg, cb);
 			}
-			
+
 			public void onCancel(Platform platform, int action) {
 				if (DEBUG) {
 					System.out.println("onCancel");
 				}
 				HashMap<String, Object> map = new HashMap<String, Object>();
-				map.put("platform", ShareSDK.platformNameToId(platform.getName()));
+				map.put("platform",
+						ShareSDK.platformNameToId(platform.getName()));
 				map.put("action", action);
 				map.put("status", 3); // Success = 1, Fail = 2, Cancel = 3
 				Message msg = new Message();
@@ -87,9 +93,9 @@ public class ShareSDKUtils {
 			}
 		};
 	}
-	
+
 	private static native void onJavaCallback(String resp);
-	
+
 	private static HashMap<String, Object> throwableToMap(Throwable t) {
 		HashMap<String, Object> map = new HashMap<String, Object>();
 		map.put("msg", t.getMessage());
@@ -109,12 +115,13 @@ public class ShareSDKUtils {
 		}
 		return map;
 	}
-	
-	public static void initSDK(final String appKey, final boolean enableStatistics) {
+
+	public static void initSDK(final String appKey,
+			final boolean enableStatistics) {
 		if (DEBUG) {
 			System.out.println("initSDK");
 		}
-		UIHandler.sendEmptyMessage(1, new Callback() {			
+		UIHandler.sendEmptyMessage(1, new Callback() {
 			@Override
 			public boolean handleMessage(Message msg) {
 				ShareSDK.initSDK(context, appKey, enableStatistics);
@@ -122,14 +129,14 @@ public class ShareSDKUtils {
 			}
 		});
 	}
-	
+
 	public static void stopSDK() {
 		if (DEBUG) {
 			System.out.println("stopSDK");
 		}
 		ShareSDK.stopSDK(context);
 	}
-	
+
 	public static void setPlatformConfig(int platformId, String configs) {
 		if (DEBUG) {
 			System.out.println("setPlatformConfig");
@@ -138,7 +145,7 @@ public class ShareSDKUtils {
 		HashMap<String, Object> conf = new Hashon().fromJson(configs);
 		ShareSDK.setPlatformDevInfo(name, conf);
 	}
-	
+
 	public static void authorize(int platformId) {
 		if (DEBUG) {
 			System.out.println("authorize");
@@ -148,7 +155,7 @@ public class ShareSDKUtils {
 		plat.setPlatformActionListener(paListaner);
 		plat.authorize();
 	}
-	
+
 	public static void removeAccount(int platformId) {
 		if (DEBUG) {
 			System.out.println("removeAccount");
@@ -157,7 +164,7 @@ public class ShareSDKUtils {
 		Platform plat = ShareSDK.getPlatform(context, name);
 		plat.removeAccount();
 	}
-	
+
 	public static boolean isValid(int platformId) {
 		if (DEBUG) {
 			System.out.println("isValid");
@@ -166,7 +173,7 @@ public class ShareSDKUtils {
 		Platform plat = ShareSDK.getPlatform(context, name);
 		return plat.isValid();
 	}
-	
+
 	public static void showUser(int platformId) {
 		if (DEBUG) {
 			System.out.println("showUser");
@@ -176,7 +183,7 @@ public class ShareSDKUtils {
 		plat.setPlatformActionListener(paListaner);
 		plat.showUser(null);
 	}
-	
+
 	public static void share(int platformId, String contentJson) {
 		if (DEBUG) {
 			System.out.println("share");
@@ -193,23 +200,43 @@ public class ShareSDKUtils {
 			paListaner.onError(plat, Platform.ACTION_SHARE, t);
 		}
 	}
-	
+
 	private static HashMap<String, Object> nativeMapToJavaMap(
 			HashMap<String, Object> content) {
 		HashMap<String, Object> map = new HashMap<String, Object>();
-		map.put("text", content.get("content"));
-		String image = (String) content.get("image");
-		if (image != null && image.startsWith("/")) {
-			map.put("imagePath", image);
-		} else if(!TextUtils.isEmpty(image)){
-			map.put("imageUrl", image);
+		if (!(content.get("content") == null)) {
+			map.put("text", content.get("content"));
 		}
-		map.put("title", content.get("title"));
-		map.put("comment", content.get("description"));
-		map.put("url", content.get("url"));
-		map.put("titleUrl", content.get("url"));
-		map.put("site",content.get("site"));
-		map.put("siteUrl",content.get("siteUrl"));
+		if (!(content.get("image") == null)) {
+			String image = (String) content.get("image");
+			if (image != null && image.startsWith("/")) {
+				map.put("imagePath", image);
+			} else if (!TextUtils.isEmpty(image)) {
+				map.put("imageUrl", image);
+			}
+		}
+		if (!(content.get("title") == null)) {
+			map.put("title", content.get("title"));
+		}
+		if (!(content.get("description") == null)) {
+			map.put("comment", content.get("description"));
+		}
+		if (!(content.get("url") == null)) {
+			map.put("url", content.get("url"));
+			map.put("titleUrl", content.get("url"));
+		}
+		if (!(content.get("site") == null)) {
+			map.put("site", content.get("site"));
+		}
+		if (!(content.get("siteUrl") == null)) {
+			map.put("siteUrl", content.get("siteUrl"));
+		}
+		if (!(content.get("musicUrl") == null)) {
+			map.put("musicUrl", content.get("musicUrl"));
+		}
+		if (!(content.get("extInfo") == null)) {
+			map.put("extInfo", content.get("extInfo"));
+		}
 		String type = (String) content.get("type");
 		if (type != null) {
 			int shareType = iosTypeToAndroidType(Integer.parseInt(type));
@@ -217,32 +244,39 @@ public class ShareSDKUtils {
 		}
 		return map;
 	}
-	
+
 	private static int iosTypeToAndroidType(int type) {
 		switch (type) {
-			case 1: return Platform.SHARE_IMAGE;
-			case 2: return Platform.SHARE_WEBPAGE;
-			case 3: return Platform.SHARE_MUSIC;
-			case 4: return Platform.SHARE_VIDEO;
-			case 5: return Platform.SHARE_APPS;
-			case 6: 
-			case 7: return Platform.SHARE_EMOJI;
-			case 8: return Platform.SHARE_FILE;
+		case 1:
+			return Platform.SHARE_IMAGE;
+		case 2:
+			return Platform.SHARE_WEBPAGE;
+		case 3:
+			return Platform.SHARE_MUSIC;
+		case 4:
+			return Platform.SHARE_VIDEO;
+		case 5:
+			return Platform.SHARE_APPS;
+		case 6:
+		case 7:
+			return Platform.SHARE_EMOJI;
+		case 8:
+			return Platform.SHARE_FILE;
 		}
-        return Platform.SHARE_TEXT;
+		return Platform.SHARE_TEXT;
 	}
-	
+
 	public static void onekeyShare(String contentJson) {
 		onekeyShare(0, contentJson);
 	}
-	
+
 	public static void onekeyShare(int platformId, String contentJson) {
 		if (DEBUG) {
 			System.out.println("OnekeyShare");
 		}
 		HashMap<String, Object> content = hashon.fromJson(contentJson);
 		HashMap<String, Object> map = nativeMapToJavaMap(content);
-		
+
 		OnekeyShare oks = new OnekeyShare();
 		if (map.containsKey("text")) {
 			oks.setText(String.valueOf(map.get("text")));
@@ -277,5 +311,15 @@ public class ShareSDKUtils {
 		}
 		oks.show(context);
 	}
-	
+
+	public static void toast(final String message) {
+		Log.e(ShareSDKUtils.class.getName(), message);
+		UIHandler.sendEmptyMessage(1, new Callback() {
+			@Override
+			public boolean handleMessage(Message msg) {
+				Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+				return true;
+			}
+		});
+	}
 }
